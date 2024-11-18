3c5cf8df98d65e0aaf0c36b59f01ad6c3677d760
Alula-az <alulaaz@gmail.com>
2024-11-18 17:26:19 +0300

    Two types banner

--- netflix-clone/src/pages/Home/home.js
+++ netflix-clone/src/pages/Home/home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Banner/banner";
+import BannerSlide from "../../components/Banner/BannerSlide";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <BannerSlide />
       <Footer />
     </>
   );
