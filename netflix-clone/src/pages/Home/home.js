13236932b011a5f9271a8022aa783108bd04ca5b
Alula-az <alulaaz@gmail.com>
2024-11-21 17:30:05 +0300

    Row and replace slide cover to the old one

--- netflix-clone/src/pages/Home/home.js
+++ netflix-clone/src/pages/Home/home.js
@@ -3,12 +3,14 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Banner from "../../components/Banner/banner";
 import BannerSlide from "../../components/Banner/BannerSlide";
+import RowList from "../../components/Rows/RowList/RowList";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <BannerSlide />
+      <Banner />
+      <RowList/>
       <Footer />
     </>
   );
--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -8,6 +8,7 @@ const requests = {
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fetchPopularTVShows: `/tv/popular?api_key=${API_KEY}&language=en-US`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99&language=en-US`,
 };
 
 export default requests;
